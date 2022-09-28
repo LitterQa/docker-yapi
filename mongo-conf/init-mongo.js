971d068a9d1895a5b9c86202581bb5764f71e7e7
LitterQa <42203637+LitterQa@users.noreply.github.com>
2022-09-28 10:10:28 +0800

    Update init-mongo.js

--- mongo-conf/init-mongo.js
+++ mongo-conf/init-mongo.js
@@ -2,12 +2,11 @@
 db.createUser({ user: 'admin', pwd: 'admin123456', roles: [ { role: "root", db: "admin" } ] });
 
 db.auth("admin", "admin123456");
- db.createUser({
- user: 'yapi',
- pwd: 'yapi123456',
- roles: [
- { role: "dbAdmin", db: "yapi" },
- { role: "readWrite", db: "yapi" }
- ]
-
- });
+db.createUser({
+user: 'yapi',
+pwd: 'yapi123456',
+roles: [
+{ role: "dbAdmin", db: "yapi" },
+{ role: "readWrite", db: "yapi" }
+]
+});
